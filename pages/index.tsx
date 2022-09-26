7a5508f7c837f308b226d8eeb82a4283e406b8c1
martispyc <burmistrs3@gmail.com>
2022-09-26 16:03:03 +0300

    came to life

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import { NextPageWithLayout } from './page';
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <section>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </section>
+  );
+};
+
+export default Home;
+
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      {page}
+    </PrimaryLayout>
+  );
+};
